2eaafc626e1478812d728c53bb83950be63cb40c
Ruben Cuello <kuelmusic@gmail.com>
2024-04-03 22:03:40 -0400

    04

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -5,13 +5,19 @@ variable using the const keyword.
 
 Examples:
 
+
+***********************************************************************/
+const arrowAvgValue = arr => {
+  let sum = 0
+  for(let num of arr){
+    sum += num
+  }
+  return sum / arr.length
+}
+
 arrowAvgValue([10, 20]); // => 15
 arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
-***********************************************************************/
-
-// Your code here 
-
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
