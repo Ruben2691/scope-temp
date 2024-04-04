cdc77fe359a82b7cd7005dbdeb483f037e22f63f
Ruben Cuello <kuelmusic@gmail.com>
2024-04-03 22:21:32 -0400

    08

--- problems/08-hidden-counter.js
+++ problems/08-hidden-counter.js
@@ -5,18 +5,23 @@ When invoked the function returned by hiddenCounter will increment the counter b
 
 Look below to see how this function is invoked:
 
+
+
+
+***********************************************************************/
+const hiddenCounter = () => {
+  let count = 0
+  return () =>{
+    return count += 1
+  }
+}
+
 let hidden1 = hiddenCounter(); //returns a function
 hidden1(); // returns 1
 hidden1(); // returns 2
 
 let hidden2 = hiddenCounter(); // returns a function
 hidden2(); // returns 1
-
-
-***********************************************************************/
-
-// Your code here 
-
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
