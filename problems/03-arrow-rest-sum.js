1db55fe397800293a26f7bacb650fbb213fdf705
Ruben Cuello <kuelmusic@gmail.com>
2024-04-03 21:59:17 -0400

    03

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -6,12 +6,19 @@ arrow function syntax.
 **Hint**: Use rest parameter syntax!
 
 Examples:
+
+***********************************************************************/
+const arrowRestSum = (...nums) =>{
+  let sum = 0
+  for(let num of nums){
+    sum += num
+  }
+  return sum
+}
+
 arrowRestSum(3,5,6); // => 14
 arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
-***********************************************************************/
-
-// Your code here 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
