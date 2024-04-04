ae7ef0c1b806537e3040a6814bcf5354259997b0
Ruben Cuello <kuelmusic@gmail.com>
2024-04-03 22:51:21 -0400

    11

--- problems/10-count-down.js
+++ problems/10-count-down.js
@@ -8,28 +8,36 @@ The function returned by countDownTimer can then be invoked n times before it
 returns a string of "Happy New Year!". Look closely at how this function is
 invoked below:
 
-Example 1:
+
+
+***********************************************************************/
+const countDownTimer = (num) => {
+
+    return () =>{
+      let count = num
+      count -= 1
+      if(count ===  0) return "Happy New Year!"
+    }
+
+}
+
+
   console.log(countDownTimer(0)); // prints "Happy New Year!"
 
-Example 2:
+
   let oneDay = countDownTimer(1); // returns a function
   console.log(oneDay()); // prints "Happy New Year!"
 
-Example 3:
+
   let twoDays = countDownTimer(2); // returns a function
   console.log(twoDays()); // returns a function
   console.log(twoDays()); // prints "Happy New Year!"
 
-Example 4:
+
   let threeDays = countDownTimer(3); // returns a function
   console.log(threeDays()); // returns a function
   console.log(threeDays()); // returns a function
   console.log(threeDays()); // prints "Happy New Year!"
-
-***********************************************************************/
-
-// Your code here 
-
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
--- problems/11-lazy-adder.js
+++ problems/11-lazy-adder.js
@@ -6,24 +6,34 @@ When the last mentioned function is invoked with a number, (thirdNum), it will
 FINALLY return a number. See below for examples!
 
 Example 1:
-let firstAdd = lazyAdder(1);
-let secondAdd = firstAdd(2);
-let sum = secondAdd(3);
-console.log(sum); // prints 6
 
-Example 2:
-let func1 = lazyAdder(10);
-let func2 = func1(20);
-let total = func2(3);
-console.log(total); // prints 33
 
 AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
 - Explain, but don't code, how you would turn your solution into a one-line
   fat-arrow function
 ***********************************************************************/
+// const lazyAdder = first =>{
+//   return (second) =>{
+//     return (third) =>{
+//       return first + second + third
+//     }
+//   }
+// }
 
-// Your code here 
+const lazyAdder = first => (second) => (third) => first + second + third
 
+
+
+let firstAdd = lazyAdder(1);
+let secondAdd = firstAdd(2);
+let sum = secondAdd(3);
+console.log(sum); // prints 6
+
+
+let func1 = lazyAdder(10);
+let func2 = func1(20);
+let total = func2(3);
+console.log(total); // prints 33
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
